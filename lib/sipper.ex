e2abb7381fb0befc1f1c10e6f52566fcca72b23a
Henrik Nyh <henrik@nyh.se>
2015-09-17 17:52:16 +0200

    Spike: get login cookie, retrieve a file

--- lib/sipper.ex
+++ lib/sipper.ex
@@ -14,7 +14,22 @@ defmodule Sipper do
 
   defp run(user: user, pw: pw) do
     IO.puts "Retrieving feed…"
-    %HTTPotion.Response{body: body, status_code: 200} = HTTPotion.get(@feed_url, basic_auth: {user, pw})
-    IO.puts body
+    %HTTPotion.Response{body: html, status_code: 200} = HTTPotion.get(@feed_url, basic_auth: {user, pw})
+    IO.puts String.length(html)  # TODO: use it…
+
+    # Figure out how to download files:
+
+    body = "username=#{user}&password=#{pw}"  # TODO: escape values
+    response = HTTPotion.post "http://elixirsips.dpdcart.com/subscriber/login", body: body, headers: ["Content-Type": "application/x-www-form-urlencoded"]
+    IO.inspect response
+
+    [_blah, raw_cookie] = response.headers |> Keyword.fetch!(:"Set-Cookie")
+    cookie = raw_cookie |> String.split(";") |> hd
+
+    IO.inspect cookie: cookie
+
+    small_markdown_file = "https://elixirsips.dpdcart.com/subscriber/download?file_id=58825"
+    response = HTTPotion.get small_markdown_file, headers: ["Cookie": cookie]
+    IO.puts response.body
   end
 end
