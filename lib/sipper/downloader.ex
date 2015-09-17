dd75c6062a55d1bb409b191c65088dee8aafa0c9
Henrik Nyh <henrik@nyh.se>
2015-09-17 20:46:15 +0200

    Cleanup

--- lib/sipper/downloader.ex
+++ lib/sipper/downloader.ex
@@ -1,20 +1,25 @@
 defmodule Sipper.Downloader do
   @subdomain "elixirsips"
   @feed_url  "https://#{@subdomain}.dpdcart.com/feed"
-  @timeout_ms 10_000  # The default 5000 times out sometimes.
+  @timeout_ms 10_000  # The default 5000 will time out sometimes.
 
   def run(user, pw) do
-    IO.puts "Retrieving feed…"
-
-    # Retrieving the feed:
-    response = HTTPotion.get(@feed_url, basic_auth: {user, pw}, timeout: @timeout_ms)
-    %HTTPotion.Response{body: html, status_code: 200} = response
-    IO.puts String.length(html)  # TODO: use it…
+    {user, pw}
+    |> get_feed_html
+    |> String.length |> IO.puts
 
     # Downloading a file (id, filename)
     {file_id, file_name} = {"1413", "003_Pattern_Matching.md"}
     url = "https://#{@subdomain}.dpdcart.com/feed/download/#{file_id}/#{file_name}"
     response = HTTPotion.get(url, basic_auth: {user, pw})
     IO.puts response.body
+  end
+
+  defp get_feed_html(auth) do
+    IO.puts "Retrieving feed…"
+
+    response = HTTPotion.get(@feed_url, basic_auth: auth, timeout: @timeout_ms)
+    %HTTPotion.Response{body: html, status_code: 200} = response
+    html
   end
 end
