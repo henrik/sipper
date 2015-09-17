2faf81a244eee545c8466856246c9df647b861fd
Henrik Nyh <henrik@nyh.se>
2015-09-17 20:01:48 +0200

    Get files without logging in
    We don't need the whole cookie thing.

--- lib/sipper/downloader.ex
+++ lib/sipper/downloader.ex
@@ -1,28 +1,20 @@
 defmodule Sipper.Downloader do
   @subdomain "elixirsips"
   @feed_url  "https://#{@subdomain}.dpdcart.com/feed"
-  @login_url "https://#{@subdomain}.dpdcart.com/subscriber/login"
+  @timeout_ms 10_000  # The default 5000 times out sometimes.
 
   def run(user, pw) do
     IO.puts "Retrieving feed…"
 
     # Retrieving the feed:
-    %HTTPotion.Response{body: html, status_code: 200} = HTTPotion.get(@feed_url, basic_auth: {user, pw})
+    response = HTTPotion.get(@feed_url, basic_auth: {user, pw}, timeout: @timeout_ms)
+    %HTTPotion.Response{body: html, status_code: 200} = response
     IO.puts String.length(html)  # TODO: use it…
 
-    # Logging in…
-    post_body = URI.encode_query(username: user, password: pw)
-    response = HTTPotion.post @login_url,
-      body: post_body,
-      headers: ["Content-Type": "application/x-www-form-urlencoded"]
-
-    # … and getting a session cookie
-    [_blah, raw_cookie] = response.headers |> Keyword.fetch!(:"Set-Cookie")
-    cookie = raw_cookie |> String.split(";") |> hd
-
-    # Downloading a file using that cookie
-    small_markdown_file = "https://elixirsips.dpdcart.com/subscriber/download?file_id=58825"
-    response = HTTPotion.get small_markdown_file, headers: ["Cookie": cookie]
+    # Downloading a file (id, filename)
+    {file_id, file_name} = {"1413", "003_Pattern_Matching.md"}
+    url = "https://#{@subdomain}.dpdcart.com/feed/download/#{file_id}/#{file_name}"
+    response = HTTPotion.get(url, basic_auth: {user, pw})
     IO.puts response.body
   end
 end
