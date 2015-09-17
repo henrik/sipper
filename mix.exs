0d0361484a9de631c3106c2a9310b92e8c887353
Henrik Nyh <henrik@nyh.se>
2015-09-17 19:49:21 +0200

    mix.exs cleanup

--- mix.exs
+++ mix.exs
@@ -2,18 +2,17 @@ defmodule Sipper.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :sipper,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     escript: [main_module: Sipper.CLI],
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :sipper,
+      version: "0.0.1",
+      elixir: "~> 1.0",
+      escript: [main_module: Sipper.CLI],
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps,
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [
       :logger,
@@ -21,15 +20,6 @@ defmodule Sipper.Mixfile do
     ]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
       {:ibrowse, github: "cmullaparthi/ibrowse", tag: "v4.1.2"},
